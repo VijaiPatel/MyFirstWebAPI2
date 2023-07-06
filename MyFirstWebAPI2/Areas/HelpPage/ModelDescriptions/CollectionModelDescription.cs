5228799843d694970517620fc617ee164ac62f43
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-07-06 13:57:16 +0100

    Add project files.

---
+++ MyFirstWebAPI2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MyFirstWebAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MyFirstWebAPI2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyFirstWebAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
